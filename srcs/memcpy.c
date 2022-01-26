cf0b531b7bcea72030ca2bcb00c000fc01888fec
dhubleur <dhubleur@student.42.fr>
2022-01-26 12:43:44 +0100

    memcpy

--- srcs/memcpy.c
+++ srcs/memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 19:12:30 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/01/26 12:15:27 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/01/26 12:28:44 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,199 @@
 static char	*test1()
 {
 	char	*str = malloc(sizeof(char) * 50);
-	char	*str2 = malloc(sizeof(char) * 51);
+	char	*str2 = malloc(sizeof(char) * 50);
+	char	*str3 = malloc(sizeof(char) * 51);
 	memset(str, 'a', 50);
-	ft_bzero(str, 10);
+	memset(str2, 'b', 50);
+	ft_memcpy(str2, str, 10);
 	for(int i = 0; i < 50; i++)
 	{
-		if(str[i] == 0)
-			str2[i] = '0';
+		if(str2[i] == 0)
+			str3[i] = '0';
 		else
-			str2[i] = '1';
+			str3[i] = str2[i];
 	}
-	str2[50] = '\0';
-	return str2;
+	str3[50] = '\0';
+	return str3;
 }
 static char	*compare1()
 {
 	char	*str = malloc(sizeof(char) * 50);
-	char	*str2 = malloc(sizeof(char) * 51);
+	char	*str2 = malloc(sizeof(char) * 50);
+	char	*str3 = malloc(sizeof(char) * 51);
 	memset(str, 'a', 50);
-	bzero(str, 10);
+	memset(str2, 'b', 50);
+	memcpy(str2, str, 10);
 	for(int i = 0; i < 50; i++)
 	{
-		if(str[i] == 0)
-			str2[i] = '0';
+		if(str2[i] == 0)
+			str3[i] = '0';
 		else
-			str2[i] = '1';
+			str3[i] = str2[i];
 	}
-	str2[50] = '\0';
-	return str2;
+	str3[50] = '\0';
+	return str3;
+}
+
+static char	*test2()
+{
+	char	*str = malloc(sizeof(char) * 50);
+	char	*str2 = malloc(sizeof(char) * 50);
+	char	*str3 = malloc(sizeof(char) * 51);
+	memset(str, 'a', 50);
+	str[5] = '\0';
+	memset(str2, 'b', 50);
+	ft_memcpy(str2, str, 10);
+	for(int i = 0; i < 50; i++)
+	{
+		if(str2[i] == 0)
+			str3[i] = '0';
+		else
+			str3[i] = str2[i];
+	}
+	str3[50] = '\0';
+	return str3;
+}
+static char	*compare2()
+{
+	char	*str = malloc(sizeof(char) * 50);
+	char	*str2 = malloc(sizeof(char) * 50);
+	char	*str3 = malloc(sizeof(char) * 51);
+	memset(str, 'a', 50);
+	str[5] = '\0';
+	memset(str2, 'b', 50);
+	memcpy(str2, str, 10);
+	for(int i = 0; i < 50; i++)
+	{
+		if(str2[i] == 0)
+			str3[i] = '0';
+		else
+			str3[i] = str2[i];
+	}
+	str3[50] = '\0';
+	return str3;
+}
+
+static char	*test3()
+{
+	char	*str = malloc(sizeof(char) * 50);
+	char	*str2 = malloc(sizeof(char) * 50);
+	char	*str3 = malloc(sizeof(char) * 51);
+	memset(str, 'a', 50);
+	memset(str2, 'b', 50);
+	ft_memcpy(str2, str, 0);
+	for(int i = 0; i < 50; i++)
+	{
+		if(str2[i] == 0)
+			str3[i] = '0';
+		else
+			str3[i] = str2[i];
+	}
+	str3[50] = '\0';
+	return str3;
+}
+static char	*compare3()
+{
+	char	*str = malloc(sizeof(char) * 50);
+	char	*str2 = malloc(sizeof(char) * 50);
+	char	*str3 = malloc(sizeof(char) * 51);
+	memset(str, 'a', 50);
+	memset(str2, 'b', 50);
+	memcpy(str2, str, 0);
+	for(int i = 0; i < 50; i++)
+	{
+		if(str2[i] == 0)
+			str3[i] = '0';
+		else
+			str3[i] = str2[i];
+	}
+	str3[50] = '\0';
+	return str3;
+}
+
+static char	*test4()
+{
+	char	*str = malloc(sizeof(char) * 50);
+	char	*str2 = malloc(sizeof(char) * 50);
+	char	*str3 = malloc(sizeof(char) * 51);
+	memset(str, 'a', 50);
+	memset(str2, 'b', 50);
+	ft_memcpy(str2, str, 50);
+	for(int i = 0; i < 50; i++)
+	{
+		if(str2[i] == 0)
+			str3[i] = '0';
+		else
+			str3[i] = str2[i];
+	}
+	str3[50] = '\0';
+	return str3;
+}
+static char	*compare4()
+{
+	char	*str = malloc(sizeof(char) * 50);
+	char	*str2 = malloc(sizeof(char) * 50);
+	char	*str3 = malloc(sizeof(char) * 51);
+	memset(str, 'a', 50);
+	memset(str2, 'b', 50);
+	memcpy(str2, str, 50);
+	for(int i = 0; i < 50; i++)
+	{
+		if(str2[i] == 0)
+			str3[i] = '0';
+		else
+			str3[i] = str2[i];
+	}
+	str3[50] = '\0';
+	return str3;
+}
+
+static char	*test5()
+{
+	char	*str = malloc(sizeof(char) * 50);
+	char	*str2 = malloc(sizeof(char) * 50);
+	char	*str3 = malloc(sizeof(char) * 51);
+	memset(str, 'a', 50);
+	str[0] = '\0';
+	memset(str2, 'b', 50);
+	ft_memcpy(str2, str, 1);
+	for(int i = 0; i < 50; i++)
+	{
+		if(str2[i] == 0)
+			str3[i] = '0';
+		else
+			str3[i] = str2[i];
+	}
+	str3[50] = '\0';
+	return str3;
+}
+static char	*compare5()
+{
+	char	*str = malloc(sizeof(char) * 50);
+	char	*str2 = malloc(sizeof(char) * 50);
+	char	*str3 = malloc(sizeof(char) * 51);
+	memset(str, 'a', 50);
+	str[0] = '\0';
+	memset(str2, 'b', 50);
+	memcpy(str2, str, 1);
+	for(int i = 0; i < 50; i++)
+	{
+		if(str2[i] == 0)
+			str3[i] = '0';
+		else
+			str3[i] = str2[i];
+	}
+	str3[50] = '\0';
+	return str3;
 }
 
 t_tests_list	*memcpy_list()
 {
 	t_tests_list	*list = init_tests_list("Memcpy");
-	add_test(list, "Classic", "ft_bzero(str, 10)", FALSE, STR_COMPARE, &test1, &compare1);
+	add_test(list, "Classic", "ft_memcpy(str2, str, 10);", FALSE, STR_COMPARE, &test1, &compare1);
+	add_test(list, "With \\0 in the copy", "ft_memcpy(str2, str, 10);", FALSE, STR_COMPARE, &test2, &compare2);
+	add_test(list, "Don't do anything", "ft_memcpy(str2, str, 0);", FALSE, STR_COMPARE, &test3, &compare3);
+	add_test(list, "Fill buffer", "ft_memcpy(str2, str, 50);", FALSE, STR_COMPARE, &test4, &compare4);
+	add_test(list, "Only \\0", "ft_memcpy(str2, str, 1);", FALSE, STR_COMPARE, &test5, &compare5);
 	return (list);
 }
--- srcs/strlen.c
+++ srcs/strlen.c
@@ -6,16 +6,12 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 18:40:47 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/01/25 19:32:35 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/01/26 12:43:33 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_tester.h"
 
-static int	test1()
-{
-	return ft_strlen(NULL);
-}
 static int	test2()
 {
 	return ft_strlen("");
@@ -35,6 +31,5 @@ t_tests_list	*strlen_list()
 	add_test(list, "Empty", "ft_strlen(\"\")", FALSE, INT_VALUE, &test2, 0);
 	add_test(list, "Classic", "ft_strlen(\"Salut !\n\")", FALSE, INT_VALUE, &test3, 8);
 	add_test(list, "Long chain", "ft_strlen(\"Salut, comment ca va ?\n Tres bien et toi ?\n\")", FALSE, INT_VALUE, &test4, 43);
-	add_test(list, "Null", "ft_strlen(NULL). The crash is allowed because the protection is not mandatory", TRUE, INT_VALUE, &test1, 0);
 	return (list);
 }
